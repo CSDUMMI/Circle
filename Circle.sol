1f62623f417265fee7ec2a70a0def543980fdd68
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-04-24 20:19:43 +0200

    exspell() function that every member can call to exspell another member, if that member didn't behave well. But the Member will only actually be exspelled if all but he himself agree.

--- Circle.sol
+++ Circle.sol
@@ -8,8 +8,11 @@ contract Circle {
         uint spendings;
         bool[] exspell_votes;
     }
+    
+    uint current_members;
     mapping (address => Member) members; // address => amount spend from the Circle
-
+    
+    
     modifier isMember {
         require(members[msg.sender].spendings > 0);
         _;
@@ -24,5 +27,21 @@ contract Circle {
     function spend(uint _amount) public isMember {
         msg.sender.transfer(_amount);
         members[msg.sender].spendings += _amount;
+    }
+    
+    function exspell(address _member) public isMember {
+        require(members[_member].spendings > 0);
+        members[_member].exspell_votes[members[msg.sender].id] = true;
+        uint votes_to_exspell = 0;
+        for(uint i = 0;i < 10; i++) {
+            if(members[_member].exspell_votes[i]) {
+                votes_to_exspell++;
+            }
+        }
+        if(votes_to_exspell == current_members-1) {
+            // All other members of the circle have
+            // to agree to exspell one of their own.
+            members[_member].spendings = 0; // hereby isMember doesn't recognize this as a Member anymore.
+        }
     }
 }
